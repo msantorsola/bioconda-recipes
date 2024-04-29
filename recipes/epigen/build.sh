187d74f5d59d8b45fd87ac4debb0bfa0b17de05f
Mariangela Santorsola <santorsola@login01.eos.medooza.lan>
2024-04-29 16:50:36 +0200

    download ext tools

--- recipes/epigen/build.sh
+++ recipes/epigen/build.sh
@@ -6,7 +6,7 @@ mkdir -p "${PREFIX}/bin"
 mkdir -p ${PREFIX}/share/epigen
 cp -r * ${PREFIX}/share/epigen
 cp $RECIPE_DIR/download_epigen_data.sh ${PREFIX}/share/epigen 
-
+cp $RECIPE_DIR/run_test_data.sh ${PREFIX}/share/epigen
 #cp -r corpora ${PREFIX}/share/epigen
 #cp -r ext ${PREFIX}/share/epigen
 
@@ -15,11 +15,12 @@ cp $RECIPE_DIR/download_epigen_data.sh ${PREFIX}/share/epigen
 
 cp -r ${PREFIX}/share/epigen/* ${PREFIX}/bin
 cp ${PREFIX}/share/epigen/download_epigen_data.sh ${PREFIX}/bin 
+cp ${PREFIX}/share/epigen/run_test_data.sh ${PREFIX}/bin
 
 chmod +x ${PREFIX}/bin/*
 chmod +x ${PREFIX}/share/epigen/*.py
 chmod +x ${PREFIX}/share/epigen/utils/*
 chmod +x ${PREFIX}/share/epigen/download_epigen_data.sh
-
+chmod +x ${PREFIX}/share/epigen/run_test_data.sh
 
 
--- recipes/epigen/download_epigen_data.sh
+++ recipes/epigen/download_epigen_data.sh
@@ -7,18 +7,18 @@ set -x
 
 if [[ -z "$1" ]]; then
     echo
-    echo "Downloads EpiGEN corpora and ext data to <outdir>"
+    echo "Download EpiGEN data to <outdir>"
     echo "Usage: $0 <outdir>"
     echo
     exit 1
 fi
 
 OUTDIR=$(readlink -f $1)
-mkdir -p $OUTDIR
+mkdir -p $OUTDIR $OUTDIR/sim $OUTDIR/ext $OUTDIR/temp $OUTDIR/ext/HAPGEN2/Darwin $OUTDIR/ext/HAPGEN2/Linux
+
+
 TMP=$(mktemp -d)
 cd $TMP
-#curl -O -L https://github.com/jiminsong/Spectacle/archive/v1.4.tar.gz > v1.4.tar.gz
-#https://github.com/biomedbigdata/epigen/archive/refs/heads/master.zip
 curl -L -o epigen.zip https://github.com/msantorsola/epigen/archive/e1c4a4ee97b7dda5beb12be2eaeb5069a479b43f.zip 
 
 #tar -xf 
@@ -27,4 +27,14 @@ cd epigen-e1c4a4ee97b7dda5beb12be2eaeb5069a479b43f
 mv $TMP/epigen-e1c4a4ee97b7dda5beb12be2eaeb5069a479b43f/ext/HAPMAP3 $OUTDIR
 mv $TMP/epigen-e1c4a4ee97b7dda5beb12be2eaeb5069a479b43f/corpora $OUTDIR
 
+echo "Download HAPGEN2"
+#linux
+wget https://mathgen.stats.ox.ac.uk/genetics_software/hapgen/download/builds/x86_64/v2.2.0/hapgen2_x86_64.tar.gz
+tar -xf  hapgen2_x86_64.tar.gz
+mv $TMP/hapgen2 $OUTDIR/ext/HAPGEN2/Linux
+
+#macos
+wget https://mathgen.stats.ox.ac.uk/genetics_software/hapgen/download/builds/macosx_intel/v2.2.0/hapgen2_macosx_intel.tar.gz
+tar -xf hapgen2_macosx_intel.tar.gz
+mv $TMP/hapgen2 $OUTDIR/ext/HAPGEN2/Linux
 
